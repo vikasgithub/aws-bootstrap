7f32f3acf49772688991daafac60263cb67b2c1a
vikas_gupta4 <vikas_gupta2@apple.com>
2020-09-20 19:49:16 -0400

    Add codebuild / codedeploy spec

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -5,6 +5,21 @@ REGION=us-east-1
 CLI_PROFILE=awsbootstrap
 
 EC2_INSTANCE_TYPE=t2.micro 
+AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile awsbootstrap \
+  --query "Account" --output text`
+CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
+
+echo -e "\n\n=========== Deploying setup.yml ==========="
+aws cloudformation deploy \
+  --region $REGION \
+  --profile $CLI_PROFILE \
+  --stack-name $STACK_NAME-setup \
+  --template-file setup.yml \
+  --no-fail-on-empty-changeset \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --parameter-overrides \
+    CodePipelineBucket=$CODEPIPELINE_BUCKET
+    
 
 # Deploy the CloudFormation template
 echo -e "\n\n=========== Deploying main.yml ==========="
@@ -23,3 +38,5 @@ if [ $? -eq 0 ]; then
     --profile awsbootstrap \
     --query "Exports[?Name=='InstanceEndpoint'].Value" 
 fi
+
+
